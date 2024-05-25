83ba3b91645a767c4e5006bb9744ebdc539e00a3
idohyeon <ehgus5026@naver.com>
2024-05-25 18:13:11 +0900

    -Setting

---
+++ src/main/java/com/example/SpringTypeConverter/SpringTypeConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringTypeConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTypeConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTypeConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringTypeConverter/SpringTypeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringTypeConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTypeConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
